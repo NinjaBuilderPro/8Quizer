69f2367cb37fa4b859d75cd681f066f6cb297099
Ninjabuilderpro <Ninjabuilderpro@users.noreply.github.com>
2024-03-10 17:20:31 -0400

    First Commit(Works in terminal)

---
+++ Constants.java
@@ -0,0 +1,4 @@
+public class Constants {
+    public static final int QUESTION_SIZE = 100;
+    public static final boolean MAKE_REQUIZ = true;
+}
---
+++ Question.java
@@ -0,0 +1,24 @@
+public class Question {
+    String m_Question;
+    String m_Answer;
+    Question(String question, String answer) {
+        m_Question = question;
+        m_Answer = answer;
+    }
+
+    public String getAnswer() {
+        return m_Answer;
+    }
+
+    public String getQuestion() {
+        return m_Question;
+    }
+
+    public void setAnswer(String m_Answer) {
+        this.m_Answer = m_Answer;
+    }
+
+    public void setQuestion(String m_Question) {
+        this.m_Question = m_Question;
+    }
+}
